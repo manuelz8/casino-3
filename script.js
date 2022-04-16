22f1c41c4ea1fff4c38cd1fd405c2c39e2b37661
Manuel Zambrano <89891295+manuelz8@users.noreply.github.com>
2022-04-16 00:57:30 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var checkbox = document.getElementById('game');
-checkbox.addEventListener("change", validaCheckbox, false);
-function validaCheckbox()
-{
-  var checked = checkbox.checked;
-  if(checked){
-    alert('checkbox1 esta seleccionado');
-  }
-}
